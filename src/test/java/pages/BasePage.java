7ff0cb49b73753d984b18209210696ea1494485e
Srishti Goel <Srishti.Goel@NIIT-Tech.com>
2019-12-05 13:38:36 +0530

    Raman BasePage BaseTest

---
+++ src/test/java/pages/BasePage.java
@@ -0,0 +1,53 @@
+package pages;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BasePage {
+	
+
+		String brows;
+		WebDriver driver;
+		Properties prop ;
+		FileInputStream fis;
+
+		public WebDriver getDriver() {
+			return driver;
+		}
+
+		public void setDriver(WebDriver driver) {
+			this.driver = driver;
+		}
+
+		BasePage() throws IOException {
+
+			fis = new FileInputStream("C:\\Users\\srishti.goel\\Favorites\\eclipse\\Feature1\\src\\main\\java\\config.properties");
+			prop = new Properties();
+			prop.load(fis);
+
+		public void initWebDriver() throws IOException
+
+		{
+			
+			brows = prop.getProperty("browser");
+
+			if (brows.equals("chrome"))
+
+			{
+				System.setProperty("webdriver.chrome.driver",prop.getProperty("ChromePath"));
+				driver = new ChromeDriver();
+			}
+
+			else
+				System.out.println("not found");
+
+		}
+
+	
+
+
+}
